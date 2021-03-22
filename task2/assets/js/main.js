6493da6f46d07dd5e23c9f99a11ec0a2ca493872
nitch193 <niteshchaurasiya193@gmail.com>
2021-03-22 16:41:51 +0530

    almost all tasks are completed only UI improvements are left

--- task2/assets/js/main.js
+++ task2/assets/js/main.js
@@ -6,7 +6,14 @@ let counter = 0;
 let slider = document.querySelector(".sliders-container");
 let next = document.querySelector(".next");
 let prev = document.querySelector(".prev");
+let mainSliders = document.querySelector(".main-sliders");
+let mainSlider = document.querySelector(".main-slider");
+let mainCounter = 0;
+let mainButtons = document.querySelectorAll(".main-ellipse");
+let mainNext = document.querySelector(".main-next");
+let mainPrev = document.querySelector(".main-prev");
 var strX;
+var mainsrtX;
 
 window.addEventListener("load", hidPopupAfterTime);
 
@@ -20,9 +27,15 @@ slider.addEventListener(
     swipemove(e);
   }, 100)
 );
-
+mainSlider.addEventListener("touchstart", swipestart);
+mainSlider.addEventListener(
+  "touchmove",
+  debounce((e) => {
+    mainSwipemove(e);
+  }, 100)
+);
 buttons[0].classList.add("show-fill-ellipse");
-
+mainButtons[0].classList.add("show-fill-ellipse");
 buttons.forEach((itm, i) => {
   itm.addEventListener("click", () => {
     counter = i;
@@ -31,7 +44,14 @@ buttons.forEach((itm, i) => {
     addFillEllipse();
   });
 });
-
+mainButtons.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    mainCounter = i;
+    moveMainSliders();
+    removeFillMainEllipse();
+    addFillMainEllipse();
+  });
+});
 next.addEventListener("click", () => {
   let num_slides = sliders.children.length - 1;
   if (num_slides > counter) {
@@ -50,8 +70,29 @@ prev.addEventListener("click", () => {
   }
 });
 
+mainNext.addEventListener("click", () => {
+  let num_main_slides = mainSliders.children.length - 1;
+  if (num_main_slides > mainCounter) {
+    ++mainCounter;
+    moveMainSliders();
+    removeFillMainEllipse();
+    addFillMainEllipse();
+  }
+});
+
+mainPrev.addEventListener("click", () => {
+  if (mainCounter >= 1) {
+    --mainCounter;
+    moveMainSliders();
+    removeFillMainEllipse();
+    addFillMainEllipse();
+  }
+});
 function swipestart(e) {
   strX = e.touches[0].clientX;
+}
+function mainSwipeStart(e) {
+  mainsrtX = e.touches[0].clientX;
 }
 
 function debounce(fun, delay) {
@@ -111,4 +152,38 @@ function removeFillEliips() {
 }
 function addFillEllipse() {
   buttons[counter].classList.add("show-fill-ellipse");
+}
+
+function moveMainSliders() {
+  let moveDistance = mainSliders.children[0].clientWidth;
+  mainSliders.style.transform = `translateX(${-moveDistance * mainCounter}px)`;
+}
+function removeFillMainEllipse() {
+  mainButtons.forEach((itm) => itm.classList.remove("show-fill-ellipse"));
+}
+function addFillMainEllipse() {
+  mainButtons[mainCounter].classList.add("show-fill-ellipse");
+}
+
+function mainSwipeMove() {
+  var touch = e.touches[0];
+  var change = mainsrtX - touch.clientX;
+  console.log(change);
+  if (change > 40) {
+    if (counter >= 5) {
+      return;
+    }
+    mainCounter++;
+    moveMainSliders();
+    removeFillMainEliipse();
+    addFillMainEllipse();
+  } else if (change < -40) {
+    if (mainCounter <= 0) {
+      return;
+    }
+    mainCounter--;
+    moveMainSliders();
+    removeFillMainEliipse();
+    addFillMainEllipse();
+  }
 }
